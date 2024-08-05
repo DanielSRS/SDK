4eba9ec26cfd0fb3a079b8feaacbc4af74414cb7
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2024-08-05 14:00:46 -0300

    refactor: encode Uint8Array in base64

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -10,9 +10,13 @@
  *
  **/
 
+const encodings =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+const _keyStr =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
 export const Base64 = {
   // private property
-  _keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
 
   // public method for encoding
   encode: function (input: string) {
@@ -37,11 +41,7 @@ export const Base64 = {
       }
 
       output =
-        output +
-        this._keyStr.charAt(enc1) +
-        this._keyStr.charAt(enc2) +
-        this._keyStr.charAt(enc3) +
-        this._keyStr.charAt(enc4);
+        output + _keyStr[enc1] + _keyStr[enc2] + _keyStr[enc3] + _keyStr[enc4];
     }
 
     return output;
@@ -55,4 +55,64 @@ export const Base64 = {
 
     return this.encode(data);
   },
+
+  fromUint8Array: base64ArrayBuffer,
 };
+
+function base64ArrayBuffer(bytes: Uint8ClampedArray | Uint8Array) {
+  var base64 = '';
+
+  var byteLength = bytes.byteLength;
+  var byteRemainder = byteLength % 3;
+  var mainLength = byteLength - byteRemainder;
+
+  let a: number;
+  let b: number;
+  let c: number;
+  let d: number;
+  let chunk: number;
+
+  // Main loop deals with bytes in chunks of 3
+  for (var i = 0; i < mainLength; i = i + 3) {
+    // Combine the three bytes into a single integer
+    chunk =
+      ((bytes[i] as number) << 16) |
+      ((bytes[i + 1] as number) << 8) |
+      (bytes[i + 2] as number);
+
+    // Use bitmasks to extract 6-bit segments from the triplet
+    a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
+    b = (chunk & 258048) >> 12; // 258048   = (2^6 - 1) << 12
+    c = (chunk & 4032) >> 6; // 4032     = (2^6 - 1) << 6
+    d = chunk & 63; // 63       = 2^6 - 1
+
+    // Convert the raw binary segments to the appropriate ASCII encoding
+    base64 +=
+      (encodings[a] as string) + encodings[b] + encodings[c] + encodings[d];
+  }
+
+  // Deal with the remaining bytes and padding
+  if (byteRemainder === 1) {
+    chunk = bytes[mainLength] as number;
+
+    a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
+
+    // Set the 4 least significant bits to zero
+    b = (chunk & 3) << 4; // 3   = 2^2 - 1
+
+    base64 += (encodings[a] as string) + encodings[b] + '==';
+  } else if (byteRemainder === 2) {
+    chunk =
+      ((bytes[mainLength] as number) << 8) | (bytes[mainLength + 1] as number);
+
+    a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
+    b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
+
+    // Set the 2 least significant bits to zero
+    c = (chunk & 15) << 2; // 15    = 2^4 - 1
+
+    base64 += (encodings[a] as string) + encodings[b] + encodings[c] + '=';
+  }
+
+  return base64;
+}
