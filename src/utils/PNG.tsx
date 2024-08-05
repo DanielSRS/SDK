94ad213b1c388e37518c1dbb75bea40c55d6c9ef
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2024-08-05 14:12:26 -0300

    refactor: add bitmap image generation as an option to png

--- src/utils/PNG.tsx
+++ src/utils/PNG.tsx
@@ -5,7 +5,11 @@ import { Body } from '../components/Text/Body';
 import { png } from './createPng';
 import { Base64 } from './base64';
 import { useState } from 'react';
-import { createCrossSectionofHSVCylinder } from './hsv';
+import {
+  createCrossSectionofHSVCylinder,
+  createCrossSectionofHSVCylinderArr,
+} from './hsv';
+import { BmpImage } from './images/BmpImage';
 
 export function PNG() {
   const [img, setImg] = useState<{
@@ -14,10 +18,11 @@ export function PNG() {
     pngH: number;
   }>();
   const generateDataPng = () => {
+    const usePng = true;
     console.log('started');
     const start = Date.now();
 
-    const res = test();
+    const res = usePng ? test() : test2();
     const end = Date.now();
     const elapsed = end - start;
     console.log('Ended in: ', elapsed);
@@ -30,7 +35,7 @@ export function PNG() {
         flex: 1,
         justifyContent: 'space-between',
         // padding: 30,
-        // backgroundColor: 'red',
+        // backgroundColor: 'black',
       }}>
       <View>
         <Body>Body</Body>
@@ -39,10 +44,12 @@ export function PNG() {
             style={{
               width: img.pngW,
               height: img.pngH,
-              borderWidth: 1,
+              // borderWidth: 1,
+              // borderRadius: img.pngW,
+              // borderColor: 'rgba(255, 255, 255, 0.1)',
               // backgroundColor: 'red',
             }}
-            src={img.dataUrl}
+            source={{ uri: img.dataUrl }}
           />
         )}
         {}
@@ -75,6 +82,46 @@ function test() {
       {
         pngData: a2 - a1,
         pngFile: a3 - a2,
+        base64png: a4 - a3,
+        dataUrl: a5 - a4,
+      },
+      null,
+      2
+    )
+  );
+  return {
+    dataUrl,
+    pngW,
+    pngH,
+  };
+}
+
+// test bmp
+function test2() {
+  const RADIUS = 128;
+  const pngW = 2 * RADIUS;
+  const pngH = 2 * RADIUS;
+  let dataUrl = 'data:image/bmp;base64,';
+
+  const a1 = Date.now();
+  const pngFile = BmpImage(pngW, pngH);
+  // const dd = String.fromCharCode.apply(null, pngData);
+  // const dd = Uint8ToString(pngData);
+  const a2 = Date.now();
+  createCrossSectionofHSVCylinderArr(RADIUS, 1, pngFile.pixelArray);
+  const a3 = Date.now();
+
+  // const base64png = Base64.fromBinaryString(Uint8ToString(pngFile.fullData));
+  const base64png = Base64.fromUint8Array(pngFile.data);
+  const a4 = Date.now();
+  dataUrl += base64png;
+  const a5 = Date.now();
+
+  console.log(
+    JSON.stringify(
+      {
+        pngFile: a2 - a1,
+        pngData: a3 - a2,
         base64png: a4 - a3,
         dataUrl: a5 - a4,
       },
