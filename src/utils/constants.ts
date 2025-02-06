c85cfdec2c730f2763483f67f083a40120365f3b
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-02-06 10:20:03 -0300

    fix: suport box shadow being true while running on paper

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,7 @@ import { Platform } from 'react-native';
 
 const RNVersion = Platform?.constants?.reactNativeVersion?.minor ?? 0;
 
-export const suportsBoxShadow = Platform.OS === 'web' || RNVersion >= 76;
+export const IS_FABRIC_ENABLED = !!(global as any)?.nativeFabricUIManager;
+
+export const suportsBoxShadow =
+  Platform.OS === 'web' || (RNVersion >= 76 && IS_FABRIC_ENABLED);
