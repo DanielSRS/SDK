3e42e63c7a244424b4a670699177f06cd00b8eee
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-02-07 17:45:31 -0300

    docs: add color picker stories

---
+++ sample/src/ColorPicker.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ColorPicker } from 'react-native-sdk';
+
+const meta = {
+  title: 'ColorPicker',
+  component: ColorPicker,
+} satisfies Meta<typeof ColorPicker>;
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {};
+
+export default meta;
