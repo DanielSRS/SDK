8f3bb90146741758649bfddc9c231db9495cfe3a
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-02-07 17:15:14 -0300

    feat: add more constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,11 +3,22 @@ import { Platform } from 'react-native';
 const RNVersion = Platform?.constants?.reactNativeVersion?.minor ?? 0;
 
 export const IS_FABRIC_ENABLED = !!(global as any)?.nativeFabricUIManager;
+const IS_MACOS = Platform.OS === 'macos';
+const IS_IOS = Platform.OS === 'ios';
+const IS_ANDROID = Platform.OS === 'android';
+const IS_WEB = Platform.OS === 'web';
+const IS_WINDOWS = Platform.OS === 'windows';
 
 export const suportsBoxShadow =
-  Platform.OS === 'web' || (RNVersion >= 76 && IS_FABRIC_ENABLED);
+  IS_WEB || (RNVersion >= 76 && IS_FABRIC_ENABLED);
 
 export const Constants = {
   IS_FABRIC_ENABLED,
   SUPORTS_BOX_SHADOW: suportsBoxShadow,
+  IS_MACOS,
+  IS_IOS,
+  IS_ANDROID,
+  IS_WEB,
+  IS_WINDOWS,
+  RN_VERSION: RNVersion,
 };
