7e56c8ef5ee64a2eea3d0e99ded71355c23d841b
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-05-12 17:46:32 -0300

    refactor: remove extra wrapper around screen

--- src/components/tab-view/tab-view.tsx
+++ src/components/tab-view/tab-view.tsx
@@ -1,35 +1,27 @@
-import { useMemo, useRef } from 'react';
-import { Styled } from '../Styled';
-import type { Route, TabViewProps } from './tab-view.types';
-import { Animated } from 'react-native';
 import { Body } from '../Text/Body';
+import { Styled } from '../Styled';
+import { Animated } from 'react-native';
+import { useMemo, useRef } from 'react';
+import type { Route, TabViewProps } from './tab-view.types';
 
-export const anim =
-  (value: Animated.Value, to: number, onEnd?: () => void) => () => {
-    Animated.spring(value, {
-      toValue: to,
-      stiffness: 1000,
-      damping: 500,
-      mass: 3,
-      overshootClamping: true,
-      restDisplacementThreshold: 0.01,
-      restSpeedThreshold: 0.01,
-      // duration: 200,
-      useNativeDriver: false,
-    }).start(s => {
-      if (s.finished) {
-        onEnd?.();
-      }
-    });
-  };
+export const anim = (value: Animated.Value, to: number) => () => {
+  Animated.spring(value, {
+    toValue: to,
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+    useNativeDriver: false,
+  }).start();
+};
 
 export function TabView<T extends Route>(props: TabViewProps<T>) {
   const { routes, renderScene } = props;
   const screenIndex = useRef(new Animated.Value(0)).current;
   const numberOfScreens = routes.length;
-  const shiftContainerWidth = `${
-    (numberOfScreens * 100) satisfies number
-  }%` as const;
+  const shiftContainerWidth = `${numberOfScreens * 100}%` as const;
   const screenContainerWidth = `${100 / numberOfScreens}%` as const;
   const maxOffset = (numberOfScreens - 1) * -100 + '%';
   const inter = useRef(
@@ -42,10 +34,7 @@ export function TabView<T extends Route>(props: TabViewProps<T>) {
   const ScreenContainer = useMemo(
     () =>
       Styled.createStyledView({
-        // borderWidth: 2,
         width: screenContainerWidth,
-        justifyContent: 'center',
-        alignItems: 'center',
       }),
     [screenContainerWidth]
   );
@@ -56,7 +45,7 @@ export function TabView<T extends Route>(props: TabViewProps<T>) {
       const Screen: () => React.ReactNode = renderScene[key];
       return (
         <ScreenContainer key={route.key}>
-          <ScreenFill>{Screen ? <Screen /> : null}</ScreenFill>
+          {Screen ? <Screen /> : null}
         </ScreenContainer>
       );
     });
@@ -91,15 +80,12 @@ export function TabView<T extends Route>(props: TabViewProps<T>) {
         ]}>
         {renderedScreens}
       </Animated.View>
-
-      {}
     </Container>
   );
 }
 
 const screensShiftContainer = {
   flex: 1,
-  backgroundColor: 'red',
   flexDirection: 'row',
 } as const;
 
@@ -114,10 +100,4 @@ const TabsContainer = Styled.createStyledView({
 
 const Tab = Styled.createStyledTouchableOpacity({
   padding: 8,
-});
-
-const ScreenFill = Styled.createStyledView({
-  flex: 1,
-  width: '100%',
-  height: '100%',
 });
